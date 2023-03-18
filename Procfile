aca4997b6efee7c1005ba8282af19e2d2c17032f
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-03-18 12:30:33 -0300

    Adicionando Procfile para o Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: bin/dev
-
+web: bundle exec rails server -p $PORT
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_002527) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_213646) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.boolean "featured"
     t.string "title"
